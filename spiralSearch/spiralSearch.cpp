4e8d57efb5b36bfb5c14f8f0d00ae43697504229
LWich <68374589+LWich@users.noreply.github.com>
2021-03-22 15:07:59 +0300

    Update spiralSearch.cpp

--- spiralSearch/spiralSearch.cpp
+++ spiralSearch/spiralSearch.cpp
@@ -1,18 +1,11 @@
 #include "spiralSearch.h"
 int spiralSearch(int size, int* mas, int element){
-	int* center = new int;
-	int* step = new int;
-	int* num = new int;
+	int* center = new int(0);
+	int* step = new int(1);
+	int* num = new int(0);
 	int* index = new int;
-	*num = 0;
-	*center = 0;
-	*step = 1;
-	if (size % 2 == 0){
-		*center = size / 2 + 1;
-	}
-	else if (size % 2 != 0){
-		*center = size / 2;
-	}
+	if (size % 2 == 0) *center = size / 2 + 1;
+	else if (size % 2 != 0) *center = size / 2;
 	*index = *center;
 	for (int i = 0; i < size; ++i){
 		if (*num % 2 != 0){
@@ -25,12 +18,8 @@ int spiralSearch(int size, int* mas, int element){
 			++* num;
 			++* step;
 		}
-		if (mas[*index] == element){
-			break;
-		}
-		else{
-			continue;
-		}
+		if (mas[*index] == element) break;
+		else continue;
 	}
 	delete num;
 	delete step;
